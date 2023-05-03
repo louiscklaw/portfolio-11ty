1d9f0db2d1c825a81be1345fd43682612fe3eb7e
louiscklaw <louiscklaw@users.noreply.github.com>
2023-05-03 15:46:32 +0800

    update portfolio-11ty,

--- scripts/start_dev.sh
+++ scripts/start_dev.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
-docker compose -f docker/docker-compose.prod.yml pull
-docker compose -f docker/docker-compose.prod.yml build
-docker compose -f docker/docker-compose.dev.yml up -d
+docker compose -f docker/docker-compose.dev.yml pull
+docker compose -f docker/docker-compose.dev.yml build
 
+docker compose -f docker/docker-compose.dev.yml up -d
 docker compose -f docker/docker-compose.dev.yml exec -it test bash
