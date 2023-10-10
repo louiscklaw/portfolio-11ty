bc64e7a73d6089f25f8328b817bc00f551c3d0c1
Louis Law <louiscklaw@users.noreply.github.com>
2023-10-10 16:09:30 +0800

    update,

--- scripts/dc_up.sh
+++ scripts/dc_up.sh
@@ -7,5 +7,7 @@ docker run -it \
   -w /app \
   -p 3001 \
   -p 8080 \
+  -v ~/.ssh/id_rsa:/root/.ssh/id_rsa:ro \
+  -v ~/.ssh/known_host:/root/.ssh/known_hosts:ro \
   --rm \
   node:16-buster bash
